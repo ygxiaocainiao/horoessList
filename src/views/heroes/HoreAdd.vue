a4c10a22009f3e3bc782ae142c0b7b79cfa8843f
DaiGuangHui <791497621@qq.com>
2019-07-08 21:08:07 +0800

    实现添加功能

--- src/views/heroes/HoreAdd.vue
+++ src/views/heroes/HoreAdd.vue
@@ -4,23 +4,50 @@
         <form>
           <div class="form-group">
             <label for="exampleInputEmail1">姓名</label>
-            <input type="text" class="form-control" id="exampleInputEmail1" placeholder="英雄名称">
+            <input v-model="formData.name" type="text" class="form-control" id="exampleInputEmail1" placeholder="英雄名称">
           </div>
           <div class="form-group">
             <label for="exampleInputPassword1">性别</label>
-            <select class="form-control">
+            <select class="form-control" v-model="formData.gender">
                 <option value="男">男</option>
                 <option value="女">女</option>
             </select>
           </div>
-          <button type="button" class="btn btn-success">添加</button>
+          <button @click="add" type="button" class="btn btn-success">添加</button>
         </form>
       </div>
 </template>
 
 <script>
+//导入axios
+import axios from 'axios'
 export default {
-    
+    data() {
+        return {
+            formData:{
+                name:'',
+                gender:'男'
+            }
+        }
+    },
+    methods: {
+        add () {
+            axios
+            .post('http://localhost:3000/heroes',this.formData)
+            .then((res)=>{
+                const status = res.status
+                if (status===201) {
+                    //添加成功跳转到列表组件
+                    this.$router.push({name:'heroes'})
+                }else {
+                    alert('添加失败')
+                }
+            })
+            .catch((err)=>{
+                alert('服务器异常'+err)
+            })
+        }
+    },
 }
 </script>
 
