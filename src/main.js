b0e4b1ca77e1376dc90cef71dbf75e5e03ea95de
DaiGuangHui <791497621@qq.com>
2019-07-08 17:01:41 +0800

    下载vue-router 导入路由,配置路由

--- src/main.js
+++ src/main.js
@@ -6,6 +6,20 @@ import './assets/css/index.css'
 
 Vue.config.productionTip = false
 
+// 导入 vue-router
+import VueRouter from 'vue-router'
+//配置vue的插件vue-router
+Vue.use(VueRouter)
+//创建路由对象
+const router = new VueRouter({
+  //配置路由规则
+  routes:[
+
+  ]
+})
+
 new Vue({
   render: h => h(App),
+  //配置路由对象
+  router
 }).$mount('#app')
